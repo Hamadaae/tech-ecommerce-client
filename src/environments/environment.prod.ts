6e633b000274999159700addfa839764dc8590a5
Hamada <gunslinger12398@gmail.com>
2025-11-11 02:08:02 +0200

    kill me

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'https://api-eta-wine.vercel.app/api',
   discordClientId: '1432691715113881680',
-  discordRedirectUri: 'https://tech-ecommerce-omega.vercel.app/auth/discord/callback',
+  discordRedirectUri: 'https://tech-ecommerce-client.vercel.app/auth/discord/callback',
   githubClientId: 'Ov23li2j9r67Hqi2suu6',
-  githubRedirectUri: 'https://tech-ecommerce-omega.vercel.app/auth/github/callback',
+  githubRedirectUri: 'https://tech-ecommerce-client.vercel.app/auth/github/callback',
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   // apiUrl: 'http://localhost:5000/api',
   apiUrl: 'https://api-eta-wine.vercel.app/api',
   discordClientId: '1432691715113881680',
-  discordRedirectUri: 'https://tech-ecommerce-omega.vercel.app/auth/discord/callback',
+  discordRedirectUri: 'https://tech-ecommerce-client.vercel.app/auth/discord/callback',
   githubClientId: 'Ov23li2j9r67Hqi2suu6',
-  githubRedirectUri: 'https://tech-ecommerce-omega.vercel.app/auth/github/callback',
+  githubRedirectUri: 'https://tech-ecommerce-client.vercel.app/auth/github/callback',
 };
